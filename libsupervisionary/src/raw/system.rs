83369b490a42830a1be81feb842edc09190d6f42
Dominic Mulligan <dominic.p.mulligan@gmail.com>
2023-02-03 20:01:42 +0000

    libsupervisionary: added System.IO bindings to libsupervisionary (#24)
    Signed-off-by: Dominic Mulligan <dominic.p.mulligan@gmail.com>

--- libsupervisionary/src/raw/system.rs
+++ libsupervisionary/src/raw/system.rs
@@ -23,4 +23,20 @@ extern "C" {
     ) -> i32;
     /// Raw ABI binding to the `System.IO.File.Close` function.
     fn __system_io_fclose(handle: u64) -> i32;
+    /// Raw ABI binding to the `System.IO.File.Write` function.
+    fn __system_io_fwrite(
+        handle: u64,
+        buffer: *const u8,
+        length: u32,
+        count: u32,
+        written: *mut u32,
+    ) -> i32;
+    /// Raw ABI binding to the `System.IO.File.Read` function.
+    fn __system_io_fread(
+        handle: u64,
+        buffer: *mut u8,
+        length: u32,
+        count: u32,
+        written: *mut u32,
+    ) -> i32;
 }
